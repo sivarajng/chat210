0749a3a9ce80a761d57f90b427c255da53cdfd64
srish <srish>
2017-04-23 22:10:19 +0530

    socket added

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,42 @@
-var app = angular.module('chatApp',[]);
+var app = angular.module('chatApp', []);
 
-app.controller('chatCtrl',function($scope){
+app.factory('socket', function () {
+    return io.connect();
 
-$scope.welcomeMessage = "Welcome to Chat 210 v0.0.1";
-$scope.msgs = [{"message":"dsdsdsds"},{"message":"dsdsds"}];
+});
+app.controller('chatCtrl', function ($scope, socket) {
 
-$scope.sendMessage = function(){
+    $scope.user;
+    $scope.toUser;
 
-$scope.msgs.push({"message":$scope.chatMessage})
-$scope.chatMessage='';
-}
+    $scope.setUser = function (user) {
+        if (user == 'siva') { $scope.toUser = 'priya'; } else { $scope.toUser = 'siva'; }
+        $scope.user = user;
+        socket.emit('setUser', $scope.user);
+        $scope.msgs.push({ "message": "Welcome " + $scope.user + " !!!", "user": $scope.user, "toUser": $scope.toUser, "time": Date.now() });
+    };
+
+    $scope.welcomeMessage = "Welcome to Chat 210 v0.0.1";
+    $scope.msgs = [];
+
+
+    $scope.sendMessage = function () {
+        $scope.msgpush = { "message": $scope.chatMessage, "user": $scope.user, "toUser": $scope.toUser, "time": Date.now() };
+        //      $scope.msgs.push($scope.msgpush);
+        $scope.chatMessage = '';
+        socket.emit('sendMessage', $scope.msgpush);
+
+    }
+
+    socket.on('connect', function () {
+        console.log('socket client connected');
+
+    });
+
+    socket.on('getMessage', function (msg) {
+        console.log('received message : ' + JSON.stringify(msg));
+        $scope.msgs.push(msg);
+        $scope.$apply();
+    });
 
 });
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var socket = require('socket.io')(server);
+var clients = [];
+var users = {};
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,12 +13,29 @@ app.use(express.static(__dirname + '/app'));
 app.set('views', __dirname + '/app');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.render('index');
 });
 
-app.listen(app.get('port'), function() {
+server.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
+});
+
+socket.on('connection', function (client) {
+  console.log('socket connected server : ', (client.id));
+  clients.push(client);
+
+  client.on('sendMessage', function (msg) {
+    console.log('socket server received sendMessage : ' + JSON.stringify(msg));
+    client.emit('getMessage', msg);
+  });
+
+  client.on('setUser', function (user) {
+    console.log('socket server received User : ' + JSON.stringify(user));
+    user[user]=client.id;
+  });
+
+
 });
 
 
